3a1b8386212cd1316aa9fbdd1dff7d61cd8d6621
IzMichael <themichael77@outlook.com>
2020-09-18 19:10:34 +1200

    Build V1

---
+++ assets/js/fpImgSpin.js
@@ -0,0 +1,7 @@
+function startspin() {
+    document.getElementById("fpImg").classList.add("spin")
+}
+
+function stopspin() {
+    document.getElementById("fpImg").classList.remove("spin")
+}
---
+++ assets/js/import-html.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.headerimport').load("/universal-assets/header.html");
+  });
+
+$(document).ready(function(){
+    $('.footerimport').load("/universal-assets/footer.html");
+  });
+
+$(document).ready(function(){
+    $('.menuimport').load("/universal-assets/menu.html");
+  });
