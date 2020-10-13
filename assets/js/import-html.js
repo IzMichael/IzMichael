104a676505871828f99f286779c5258ea8a3be56
IzMichael <themichael77@outlook.com>
2020-10-13 14:20:17 +1300

    Merge JS

--- assets/js/import-html.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('#bioInsert').load("/config/bio.txt");
-});
-
-$(document).ready(function () {
-    $('#descInsert').load("/config/description.txt");
-});
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function pulseSocial() {
+    var socialMenu = document.getElementById("socialMenuI")
+    socialMenu.classList.add("pulse")
+    await sleep(10000);
+    socialMenu.classList.remove("pulse")
+}
+
+$(document).ready(function () {
+    $('#bioInsert').load("/config/bio.txt");
+});
+
+$(document).ready(function () {
+    $('#descInsert').load("/config/description.txt");
+});
+
 async function setPanelHome(oldPanel) {
     var sidebar = document.getElementById('sidebarParent');
     var attributionPanel = document.getElementById('attributionPanel');
@@ -25,25 +44,4 @@ async function setPanelLinks() {
     await sleep(10);
     sidebar.classList.add('-translate-y-full');
     console.log("Showing Links Panel");
-}
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-
-
-
-
-
-
-
-function showAttr() {
-    var sidebar = document.getElementById('sidebarParent');
-    sidebar.classList.add('-translate-y-full');
-}
-
-function hideAttr() {
-    var sidebar = document.getElementById('sidebarParent');
-    sidebar.classList.remove('-translate-y-full');
 }
