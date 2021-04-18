34f530594c11d3143745fafb96a416477d34f893
IzMichael <themichael77@outlook.com>
2021-04-19 09:30:13 +1200

    Update site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,58 @@
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+var questions = [
+    {"question":"7 + 4","answer":"11"},
+    {"question":"9 + 3","answer":"12"},
+    {"question":"7 + 8","answer":"15"},
+    {"question":"2 + 3","answer":"5"},
+    {"question":"5 + 3","answer":"8"},
+    {"question":"11 + 4","answer":"15"},
+    {"question":"6 + 8","answer":"12"}
+]
+var set = questions[Math.floor(Math.random() * questions.length)];
+var question = set.question;
+var answer = set.answer;
+
+var nbox = document.getElementById('name');
+var ebox = document.getElementById('email');
+var sbox = document.getElementById('subject');
+var mbox = document.getElementById('message');
+
+var qbox = document.getElementById('botQ');
+var abox = document.getElementById('botAns');
+
+qbox.innerHTML += question + '?';
+
+function run() {
+    if (nbox.value === '') {
+        pulse(nbox);
+    }
+    if (ebox.value === '') {
+        pulse(ebox);
+    }
+    if (sbox.value === '') {
+        pulse(sbox);
+    }
+    if (mbox.value === '') {
+        pulse(mbox);
+    }
+    if (abox.value === answer) {
+        document.theForm.submit();
+    } else {
+        pulse(abox);
+    }
 }
 
-async function pulseSocial() {
-    var socialMenu = document.getElementById("socialMenuI")
-    socialMenu.classList.add("pulse")
-    await sleep(10000);
-    socialMenu.classList.remove("pulse")
+async function pulse(el) {
+    el.classList.add('incorrect')
+    await sleep(3)
+    el.classList.remove('incorrect')
 }
 
-$(document).ready(function () {
-    $('#bioInsert').load("/config/bio.txt");
+document.getElementById('form').addEventListener("keydown", function (e) {
+    if (e.key === 'Enter') {
+    run();
+    }
 });
 
-$(document).ready(function () {
-    $('#descInsert').load("/config/description.txt");
-});
-
-async function setPanelHome(oldPanel) {
-    var sidebar = document.getElementById('sidebarParent');
-    var attributionPanel = document.getElementById('attributionPanel');
-    var linksPanel = document.getElementById('linksPanel');
-    sidebar.classList.remove('-translate-y-full');
-    await sleep(1000);
-    attributionPanel.classList.add('hidden');
-    linksPanel.classList.add('hidden');
-    console.log("Hidden Panel");
-}
-
-async function setPanelAttr() {
-    var sidebar = document.getElementById('sidebarParent');
-    var attributionPanel = document.getElementById('attributionPanel');
-    attributionPanel.classList.remove('hidden');
-    await sleep(10);
-    sidebar.classList.add('-translate-y-full');
-    console.log("Showing Attr Panel");
-}
-
-async function setPanelLinks() {
-    var sidebar = document.getElementById('sidebarParent');
-    var linksPanel = document.getElementById('linksPanel');
-    linksPanel.classList.remove('hidden');
-    await sleep(10);
-    sidebar.classList.add('-translate-y-full');
-    console.log("Showing Links Panel");
+function sleep(s) {
+    return new Promise(resolve => setTimeout(resolve, (s * 1000)));
 }
