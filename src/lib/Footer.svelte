3d80446fb4137f0ca696b55cefea057abe54a2aa
IzMichael <themichael77@outlook.com>
2022-11-05 19:55:38 +1300

    Redesign

--- src/lib/Footer.svelte
+++
@@ -1,3 +0,0 @@
-<div class="flex flex-row items-center justify-center px-2 py-10 snap-center">
-    <a href="https://izmichael.com/" class="flex-1 text-4xl text-center">Built by IzMichael - &copy; IzMichael 2022</a>
-</div>
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 // https://github.com/jorgegorka/svelte-router/blob/master/README.md
 
 import Index from './routes/Index.svelte';
-import NotFound from './routes/NotFound.svelte';
 
 const routes = [
     {
@@ -10,7 +9,7 @@ const routes = [
     },
     {
         name: '404',
-        component: NotFound
+        redirectTo: '/'
     }
 ]
 
