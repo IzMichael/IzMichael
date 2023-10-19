84b7fda10e7506ad26a76e0afbcf644bbe3d532c
IzMichael <michael@izmichael.com>
2023-10-20 00:47:58 +1300

    Add new projects - AU Tracker, Yeelight App. Add deprecation date handling

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -32,6 +32,23 @@
             created: 2021,
             status: 'Actively Maintained',
         },
+        {
+            name: 'Among Us Tracker',
+            link: 'https://amongus.izmichael.com/',
+            description:
+                'Among Us Tracker is an interactive interface for tracking suspicion levels, vents, and task locations in the social deduction game Among Us.',
+            created: 2020,
+            deprecated: 2020,
+            status: 'Unmaintained',
+        },
+        {
+            name: 'Yeelight App',
+            link: 'https://github.com/IzMichael/Yeelight-App',
+            description: 'Yeelight App is a Windows 10 application for controlling Yeelight branded smart lighting.',
+            created: 2021,
+            deprecated: 2021,
+            status: 'Unmaintained',
+        },
     ];
 
     interface Project {
@@ -39,6 +56,7 @@
         link: string;
         description: string;
         created: number;
+        deprecated?: number;
         status: 'Actively Maintained' | 'Unmaintained' | 'Killed';
     }
 
@@ -57,7 +75,10 @@
             <a class="block w-full rounded-xl bg-white p-5 shadow-lg" href={project.link}>
                 <h3 class="font-bold">{project.name}</h3>
                 <p class="mb-2 mt-1 text-sm">{project.description}</p>
-                <p class="text-sm text-gray-500">c. {project.created} - {project.status}</p>
+                <p class="text-sm text-gray-500">
+                    c. {project.created}
+                    {project.deprecated ? ', d. ' + project.deprecated : ''} - {project.status}
+                </p>
             </a>
         {/each}
     </div>
@@ -66,7 +87,10 @@
             <a class="block w-full rounded-xl bg-white p-5 shadow-lg" href={project.link}>
                 <h3 class="font-bold">{project.name}</h3>
                 <p class="mb-2 mt-1 text-sm">{project.description}</p>
-                <p class="text-sm text-gray-500">c. {project.created} - {project.status}</p>
+                <p class="text-sm text-gray-500">
+                    c. {project.created}
+                    {project.defunct ? ', d. ' + project.defunct : ''} - {project.status}
+                </p>
             </a>
         {/each}
     </div>
