964ffc88e4bb261ad118cae3bf83f8922468da39
IzMichael <michael@izmichael.com>
2024-08-13 13:09:19 +1200

    Fix fonts

--- src/lib/Container.svelte
+++ src/lib/Container.svelte
@@ -12,7 +12,7 @@
     <div class="flex w-full flex-row items-end justify-start">
         <h2 class="block w-fit rounded-t-2xl bg-brand-senary px-10 pt-2 text-lg font-bold">{heading}</h2>
         <div class="h-3 w-3 bg-brand-senary">
-            <span class="block h-full w-full rounded-bl-2xl bg-slate-200 p-1" />
+            <span class="block h-full w-full rounded-bl-2xl bg-brand-quinary p-1" />
         </div>
     </div>
     <div
